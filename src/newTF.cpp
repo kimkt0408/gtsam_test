720ec8b33ba9b849e13f519aa20720810e9b76d0
Kitae Kim <kimkt0408@gmail.com>
2023-11-29 13:32:17 -0500

    Publish new TF ("base_link2")
    For depth_clustering projection

--- src/newTF.cpp
+++ src/newTF.cpp
@@ -1,30 +1,31 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
-#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseStamped.h>
 
-// Odometry callback function
-void odometryCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+// PoseStamped callback function
+void poseStampedCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
     static tf::TransformBroadcaster br;
     tf::Transform transform;
-    
+
     // Set the position of the transform
-    transform.setOrigin(tf::Vector3(msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z));
+    transform.setOrigin(tf::Vector3(msg->pose.position.x, msg->pose.position.y, msg->pose.position.z));
     
     // Set the orientation of the transform
-    tf::Quaternion q(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+    tf::Quaternion q(msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z, msg->pose.orientation.w);
     transform.setRotation(q);
 
-    // Broadcasting the transform from "odom" to "base_link2"
-    br.sendTransform(tf::StampedTransform(transform, ros::Time(0), "map", "base_link2"));
+    // Broadcasting the transform from "base_link" to "base_link2"
+    // br.sendTransform(tf::StampedTransform(transform, msg->header.stamp, "map", "base_link2"));
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "base_link2"));
 }
 
 int main(int argc, char** argv){
-    ros::init(argc, argv, "newTf_broadcaster");
+    ros::init(argc, argv, "new_tf_broadcaster");
 
     ros::NodeHandle node;
     
-    // Subscribe to the odometry topic
-    ros::Subscriber sub = node.subscribe("gtsam/optimized_pose_2", 10, &odometryCallback);
+    // Subscribe to the PoseStamped topic
+    ros::Subscriber sub = node.subscribe("gtsam/optimized_pose_2", 10, &poseStampedCallback);
 
     ros::spin();
     return 0;
